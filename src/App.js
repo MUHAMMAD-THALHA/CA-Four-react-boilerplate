596939ef2890867a3678f5b63afbf3b8ebe89b8c
MUHAMMAD24THALHASEP2005 <muhammadthalhasep@gmail.com>
2024-02-01 01:37:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import "./App.css";
-import questions from "./questions";
 import Result from "./components/Result";
 import QuestionBox from "./components/QuestionBox";
-
-
+import Homepage from "./components/homepage";
+import { AppContext } from "./components/Context";
 
 function App() {
 
+//destructuring isDark from Context
+  const { isDark, setisDark } = useContext(AppContext);
+
+//toggle between isDark and !isDark
+
+  const toggleDarkMode = () => {
+    setisDark(!isDark);
+  };
+
   return (
-    <div>
-      
+    //changing the backgroundcolor on the basis of isDark
+    <body style={{backgroundColor:isDark?"black":"white",height:'100vh'}}>
+    <div style={{color:isDark?"white":"black"}}>
+      <div style={{textAlign:"end",width:"80%"}}>
+      <button className="toggle"
+        style={{ backgroundColor:isDark?"white":"black",color:isDark?"black":"white"}}
+        onClick={toggleDarkMode}
+      >
+        {isDark?"Light Mode" :"Dark Mode"}
+      </button>
+      </div>
+     {/* <QuestionBox /> */}
+     {/* <Result /> */}
+     <Homepage />
     </div>
+    </body>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Context from './components/Context';
 
 ReactDOM.render(
   <div>
+    <Context>
     <App />
+    </Context>
   </div>,
   document.getElementById('root')
 );
